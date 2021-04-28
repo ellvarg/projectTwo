972a516ada4423e765dbd9c4a0571d9e5c14081a
João Lucas Rezende <ellvarg0d@gmail.com>
2021-04-28 16:07:49 -0300

    Update Application.java

--- src/main/java/apps/Application.java
+++ src/main/java/apps/Application.java
@@ -7,8 +7,9 @@ public class Application {
     public static void main(String[] args) {
 
 
-         //preferi entregar atrasado e funcionando, ao invez de entregar algo quebrado, peço desculpas
-
+        //Desculpas pelo atraso, priorizei em entregar um trabalho aonde eu tivesse entendimento do que estava fazendo
+        // ele não está completo mas estou trabalhando em terminar e entender o projeto como um tdo, continuarem dando
+        //commits no futuro.
 
         //Call the Menu
         MainMenu.mainMenu();
