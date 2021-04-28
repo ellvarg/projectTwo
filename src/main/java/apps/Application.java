8261fc5c34a6a8edb0a425d22a454b7caa544ea0
João Lucas Rezende <ellvarg0d@gmail.com>
2021-04-28 18:55:59 -0300

    Update Application.java

--- src/main/java/apps/Application.java
+++ src/main/java/apps/Application.java
@@ -6,7 +6,6 @@ public class Application {
 
     public static void main(String[] args) {
 
-
         //Desculpas pelo atraso, priorizei em entregar um trabalho aonde eu tivesse entendimento do que estava fazendo
         // ele não está completo mas estou trabalhando em terminar e entender o projeto como um tdo, continuarem dando
         //commits no futuro.
